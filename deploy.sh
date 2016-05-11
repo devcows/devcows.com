1905db8e26f0a6a0e1b363a968716958f1723d12
Adrian Moreno <adrian@morenomartinez.com>
2016-05-11 16:56:55 +0200

    Fixes travis config

--- deploy.sh
+++ deploy.sh
@@ -56,12 +56,6 @@ ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
 ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
 ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
 openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in deploy_key.enc -out deploy_key -d
-
-if [ ! -f deploy_key ]; then
-  echo "Could not decrypt deploy key."
-  exit 1
-fi
-
 chmod 600 deploy_key
 eval `ssh-agent -s`
 ssh-add deploy_key
