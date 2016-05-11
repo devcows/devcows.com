03c4717fa0e7cdb25d8fcebd61b572e5e773785b
Adrian Moreno <adrian@morenomartinez.com>
2016-05-11 16:39:28 +0200

    Checks deploy key decryption

--- deploy.sh
+++ deploy.sh
@@ -56,6 +56,12 @@ ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
 ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
 ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
 openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in deploy_key.enc -out deploy_key -d
+
+if [ $? != 0 ]; then
+  echo "Could not decrypt deploy key."
+  exit 1
+fi
+
 chmod 600 deploy_key
 eval `ssh-agent -s`
 ssh-add deploy_key
