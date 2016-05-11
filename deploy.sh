ea3acca6ee87ed3e1fdb22fae4fb6fbef8f4ab4b
Adrian Moreno <adrian@morenomartinez.com>
2016-05-11 16:53:49 +0200

    Fixes check

--- deploy.sh
+++ deploy.sh
@@ -57,7 +57,7 @@ ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
 ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
 openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in deploy_key.enc -out deploy_key -d
 
-if [ $? != 0 ]; then
+if [ ! -f deploy_key ]; then
   echo "Could not decrypt deploy key."
   exit 1
 fi
