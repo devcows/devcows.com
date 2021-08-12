e3c3c5b01847284b3a5d1d22ce5b827ed0287c1f
ryanfox1985 <ryanfox1985@users.noreply.github.com>
2021-08-12 08:46:50 +0000

    deploy: 0fdff9b3e66941b38a6c5f412ff2b66cf490c785

--- js/gmaps.init.js
+++ js/gmaps.init.js
@@ -1,15 +1,40 @@
-$( document ).ready(function() {
-    map();
-});
+/* global GMaps: true */
 
-function map() {
-  if( $('#map').length ) {
-    var lat = $("#gmap-lat").val();
-    var lng = $("#gmap-lng").val();
-    var image = '/img/marker.png';
+$(document).ready(function () {
+  map()
+})
 
-    var styles = [{"featureType": "landscape", "stylers": [{"saturation": -100}, {"lightness": 65}, {"visibility": "on"}]}, {"featureType": "poi", "stylers": [{"saturation": -100}, {"lightness": 51}, {"visibility": "simplified"}]}, {"featureType": "road.highway", "stylers": [{"saturation": -100}, {"visibility": "simplified"}]}, {"featureType": "road.arterial", "stylers": [{"saturation": -100}, {"lightness": 30}, {"visibility": "on"}]}, {"featureType": "road.local", "stylers": [{"saturation": -100}, {"lightness": 40}, {"visibility": "on"}]}, {"featureType": "transit", "stylers": [{"saturation": -100}, {"visibility": "simplified"}]}, {"featureType": "administrative.province", "stylers": [{"visibility": "off"}]}, {"featureType": "water", "elementType": "labels", "stylers": [{"visibility": "on"}, {"lightness": -25}, {"saturation": -100}]}, {"featureType": "water", "elementType": "geometry", "stylers": [{"hue": "#ffff00"}, {"lightness": -25}, {"saturation": -97}]}];
-    map = new GMaps({
+function map () {
+  if ($('#map').length) {
+    var lat = $('#gmap-lat').val()
+    var lng = $('#gmap-lng').val()
+    var direction = $('#gmap-dir').val()
+    var image = $('#gmap-marker').val()
+
+    var styles =
+      [
+        {
+          'featureType': 'landscape', 'stylers': [{'saturation': -100}, {'lightness': 65}, {'visibility': 'on'}]
+        }, {
+          'featureType': 'poi', 'stylers': [{'saturation': -100}, {'lightness': 51}, {'visibility': 'simplified'}]
+        }, {
+          'featureType': 'road.highway', 'stylers': [{'saturation': -100}, {'visibility': 'simplified'}]
+        }, {
+          'featureType': 'road.arterial', 'stylers': [{'saturation': -100}, {'lightness': 30}, {'visibility': 'on'}]
+        }, {
+          'featureType': 'road.local', 'stylers': [{'saturation': -100}, {'lightness': 40}, {'visibility': 'on'}]
+        }, {
+          'featureType': 'transit', 'stylers': [{'saturation': -100}, {'visibility': 'simplified'}]
+        }, {
+          'featureType': 'administrative.province', 'stylers': [{'visibility': 'off'}]
+        }, {
+          'featureType': 'water', 'elementType': 'labels', 'stylers': [{'visibility': 'on'}, {'lightness': -25}, {'saturation': -100}]
+        }, {
+          'featureType': 'water', 'elementType': 'geometry', 'stylers': [{'hue': '#ffff00'}, {'lightness': -25}, {'saturation': -97}]
+        }
+      ]
+
+    var map = new GMaps({
       el: '#map',
       lat: lat,
       lng: lng,
@@ -25,16 +50,22 @@ function map() {
       scrollwheel: false,
       draggable: false,
       styles: styles
-    });
+    })
 
     map.addMarker({
       lat: lat,
       lng: lng,
-      icon: image/* ,
-       title: '',
-       infoWindow: {
-       content: '<p>HTML Content</p>'
-       }*/
-    });
+      icon: image,
+      click: function (e) {
+        // when we get an address with spaces ...
+        var url = 'https://maps.google.com?daddr=' + direction.split('match').join('replace')
+        window.open(url, '_blank')
+      },
+      title: direction
+      /* ,
+      infoWindow: {
+      content: '<p>HTML Content</p>'
+      } */
+    })
   }
 }
